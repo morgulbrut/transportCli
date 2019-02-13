56920c6677c8ddc834554cb7ea3ea2a07f367233
Tillo Bosshart <tillo.bosshart@kubeg.ch>
2019-02-13 17:07:40 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+Copyright © 2019 morgulbrut
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the LICENSE file or
+ http://www.wtfpl.net/ for more details.
+*/
+
+package main
+
+import "github.com/morgulbrut/transportCli/cmd"
+
+func main() {
+	cmd.Execute()
+}
