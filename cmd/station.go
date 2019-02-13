af17b4fc6b1307e52e12e2127350b17f604a48b0
Tillo Bosshart <tillo.bosshart@kubeg.ch>
2019-02-13 17:18:20 +0100

    station WIP commit

--- cmd/station.go
+++ cmd/station.go
@@ -17,15 +17,15 @@ import (
 // stationCmd represents the station command
 var stationCmd = &cobra.Command{
 	Use:   "station",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Returns the next connections leaving from a specific location.",
+	Long: `Returns the next connections leaving from a specific location.
+	
+	Example: transportCli station Bern
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("station called")
+		//fmt.Println("station called")
+		fmt.Printf("http://transport.opendata.ch/v1/stationboard?station=%s\n", args[0])
 	},
 }
 
