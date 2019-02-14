d633d28c95d0620326f6adacecf6d81df5e745d8
Tillo <tillo.bosshart@gmail.com>
2019-02-14 22:00:02 +0100

    License blah

--- webreq/webreq.go
+++ webreq/webreq.go
@@ -1,6 +1,15 @@
+/*
+Copyright © 2019 morgulbrut
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the LICENSE file or
+ http://www.wtfpl.net/ for more details.
+*/
+
 package webreq
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
